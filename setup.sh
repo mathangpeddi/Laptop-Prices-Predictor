fd7e05c14dc52597023140ea143180f9c25bdc9c
Mathang Peddi <53903323+mathangpeddi@users.noreply.github.com>
2020-10-12 01:26:06 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 mkdir -p ~/.streamlit/
-
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config .toml
+" > ~/.streamlit/config.toml
